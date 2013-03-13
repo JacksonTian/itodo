d0bcf785c28169fda48a6ca819fa0d244ed2fe52
Jackson Tian <shyvo1987@gmail.com>
2013-03-13 15:09:31 +0800

    完善测试

--- test/test.js
+++ test/test.js
@@ -1,11 +1,21 @@
-var Walker = require("/Users/jacksontian/git/walk_do");
-//源文件
-var source = '/Users/jacksontian/git/';
-var walker = new Walker();
-//同步深度遍历文件夹
-walker.on('walk', function (_path) {
+var path = require('path');
+var should = require('should');
+var itodo = require('../');
+
+describe('itodo', function () {
+  it('should ok', function (done) {
+    itodo.process(path.join(__dirname, './fixtures/'), function (err, list) {
+      should.not.exist(err);
+      list.should.have.length(2);
+      list[0].should.have.property('type', 'TODO');
+      list[0].should.have.property('item', '源文件');
+      list[0].filename.should.include('fixtures/1.js');
+      list[0].should.have.property('lineno', 2);
+      list[1].should.have.property('type', 'FIXME');
+      list[1].should.have.property('item', '同步深度遍历文件夹');
+      list[1].filename.should.include('fixtures/1.js');
+      list[1].should.have.property('lineno', 5);
+      done();
+    });
+  });
 });
-walker.on('end', function () {
-  console.log('finished');
-});
-walker.walk(source);
