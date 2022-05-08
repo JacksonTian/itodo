8a18affb8e18ea266150d93d8d83cdb04a3372a7
jasonh <appleoxxo@gmail.com>
2022-05-08 08:36:17 +0800

    tab to spaces

--- test/itodo.test.js
+++ test/itodo.test.js
@@ -38,11 +38,11 @@ describe('itodo', function () {
       path.join(__dirname, './fixtures/filterDir'),
       ['a','build']
     );
-		assert.strictEqual(list.length, 2);
+    assert.strictEqual(list.length, 2);
     assert.strictEqual(list[0].type, 'TODO');
     assert.strictEqual(list[0].item, '源文件');
-		assert.ok(list[0].filename.endsWith('fixtures/filterDir/dist/1.js'));
-		assert.strictEqual(list[0].lineno, 2);
+    assert.ok(list[0].filename.endsWith('fixtures/filterDir/dist/1.js'));
+    assert.strictEqual(list[0].lineno, 2);
     assert.strictEqual(list[1].type, 'FIXME');
     assert.strictEqual(list[1].item, '同步深度遍历文件夹');
     assert.ok(list[1].filename.endsWith('fixtures/filterDir/dist/1.js'));
