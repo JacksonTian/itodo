22532e01a5b1f7ebc0a9bcf9b6826fb597660cef
jasonh <appleoxxo@gmail.com>
2022-05-08 08:29:48 +0800

    fix test

--- test/itodo.test.js
+++ test/itodo.test.js
@@ -36,16 +36,16 @@ describe('itodo', function () {
   it('should filter directories optionally', async function () {
     const list = await itodo.process(
       path.join(__dirname, './fixtures/filterDir'),
-      ['a','build','dist']
+      ['a','build']
     );
-    assert.strictEqual(list.length, 2);
+		assert.strictEqual(list.length, 2);
     assert.strictEqual(list[0].type, 'TODO');
     assert.strictEqual(list[0].item, '源文件');
-    assert.ok(list[0].filename.endsWith('fixtures/filterDir/node_modules/1.js'));
-    assert.strictEqual(list[0].lineno, 2);
+		assert.ok(list[0].filename.endsWith('fixtures/filterDir/dist/1.js'));
+		assert.strictEqual(list[0].lineno, 2);
     assert.strictEqual(list[1].type, 'FIXME');
     assert.strictEqual(list[1].item, '同步深度遍历文件夹');
-    assert.ok(list[1].filename.endsWith('fixtures/filterDir/node_modules/1.js'));
+    assert.ok(list[1].filename.endsWith('fixtures/filterDir/dist/1.js'));
     assert.strictEqual(list[1].lineno, 5);
   });
 });
